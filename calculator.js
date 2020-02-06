66e86cb2a3dfd483e5263d10ef3d5ce04c205c5d
Brannon Young <youngbrann@gmail.com>
2020-02-06 07:10:04 -0500

    minor CoL updates

--- calculator.js
+++ calculator.js
@@ -1,5 +1,7 @@
 let inputArea = document.querySelector('.calculator-input-area');
 let textField = document.querySelector('.calculator-text-field');
+let number = 0;
+let number2 = 0;
 let total = 0;
 let sum;
 
@@ -7,31 +9,56 @@ textField.textContent = 0;
 
 inputArea.addEventListener('click', (e) => {
     target = event.target;
-    if (target.matches('.number') && !(target.matches.textContent == '0')){
-        console.log('number reached');
-        let num = target.textContent;
-        addTotal(num);
-        console.log(total);
-    }
-
-    if (target.matches('plus')){
-
-    }
+    let x = 0;
+    x = getNumbers(target);
+    
+    console.log(`X equals ${x}`);
+    addNumbersToNumber(x)
+    numberToTextField();
+    doPlus(target);
 })
 
-function addTotal(num){
-    if(num == 0){
-        return undefined;
+function getNumbers(targ){
+    if (targ.matches('.number') && targ.matches.textContent != '0'){
+        console.log('number');
+        let num = target.textContent;
+        console.log(`number is ${num}`);
+        return num;
     }
+}
+
+function addNumbersToNumber(num){
+    if (number == 0){number = num;}
+    if (number != 0){ number += num;}
+}
+
+function doPlus(targ){
+    if (target.matches('.plus')){
+        console.log('plus');
+    }
+}
+
+function addNumber(num){
+    number += num;
+}
+
+function numberToTextField(){
     clearTextField();
+    textField.textContent = number;
+}
+
+function addTotal(num){
+    // if(num == 0){
+    //     return undefined;
+    // }
+    total += num;
     updateTextField();
-    return total += num;
 }
 
 function updateTextField() {
-    textField.textContent = total;
+    textField.textContent = number;
 }
 
 function clearTextField() {
-    textField.textContent = ' ';
+    textField.textContent = '';
 }
